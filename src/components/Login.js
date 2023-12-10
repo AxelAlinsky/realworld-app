5067d6f5deca7d9228fe0355b9477ea210c912b5
Axel Alinsky <matthewkenn00@gmail.com>
2023-12-10 18:29:59 +0700

    Update Login button in Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -77,8 +77,8 @@ const Login = () => {
           <input type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
           <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
 
-          <button type="submit" disabled={isLoading}>
-          {isLoading ? 'Loading...' : 'Login'} {/* Conditional rendering */}
+          <button type="submit" disabled={isLoading} data-testid="login-button">
+            {isLoading ? 'Loading...' : 'Login'} {/* Conditional rendering */}
           </button>
           
           {message && <p className="error-message">{message}</p>}
