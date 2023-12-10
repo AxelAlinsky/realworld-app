efc8ad4014da62a9d42614b8bffd54aacd8f08ad
Axel Alinsky <matthewkenn00@gmail.com>
2023-12-10 11:52:40 +0700

    Add loading state to Login and Register forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,7 @@ const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false); // New state for loading
   const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
   const navigate = useNavigate();
 
@@ -18,6 +19,7 @@ const Login = () => {
         return;
     }
 
+    setIsLoading(true); // Start loading
     try {
       const response = await fetch('https://realworldapp-fbe9683d7507.herokuapp.com/login', {
         method: 'POST',
@@ -47,6 +49,8 @@ const Login = () => {
     } catch (error) {
         console.error('There was an error:', error);
         setMessage('An error occurred during login');
+    } finally {
+      setIsLoading(false); // Stop loading
     }
 };
 
@@ -65,21 +69,25 @@ const Login = () => {
   }
 
   return (
-    <div className="login-container">
-      <form onSubmit={handleLogin} className="input-form">
-        <input type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
-        <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
-        <button type="submit">Login</button>
-        {message && <p className="error-message">{message}</p>}
-      </form>
-      <p>
-        Don't have an account? <Link to="/register">Register here</Link>
-      </p>
-      <div className="alert-footer">
-        <span className="alert-icon">&#9888;</span> {/* Example: Exclamation mark icon */}
-        <p>Please note: This is a demo application. Avoid using real personal details for login or registration.</p>
+      <div className="login-container">
+        <form onSubmit={handleLogin} className="input-form">
+          <input type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
+          <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+
+          <button type="submit" disabled={isLoading}>
+          {isLoading ? 'Loading...' : 'Login'} {/* Conditional rendering */}
+          </button>
+          
+          {message && <p className="error-message">{message}</p>}
+        </form>
+        <p>
+          Don't have an account? <Link to="/register">Register here</Link>
+        </p>
+        <div className="alert-footer">
+          <span className="alert-icon">&#9888;</span> {/* Example: Exclamation mark icon */}
+          <p>Please note: This is a demo application. Avoid using real personal details for login or registration.</p>
+        </div>
       </div>
-    </div>
     
   );
 };
